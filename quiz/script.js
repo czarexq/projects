36a993255d3f1ce0e3905ab6e5769ebb4f1f3d49
Adam Czarycki <adam.czarycki1@gmail.com>
2026-05-12 17:24:15 +0200

    Connect basic script

---
+++ quiz/script.js
@@ -0,0 +1,19 @@
+let number = document.getElementById("question_number")
+let question = document.getElementById("question")
+let points = 0
+
+function one() {
+
+}
+
+function two() {
+  
+}
+
+function three() {
+  
+}
+
+function four() {
+  
+}
