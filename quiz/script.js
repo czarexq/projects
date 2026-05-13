59bfecb6eef031228a80a4aa625b38701f7f3d77
Adam Czarycki <adam.czarycki1@gmail.com>
2026-05-13 22:01:58 +0200

    fix script

--- quiz/script.js
+++ quiz/script.js
@@ -1,19 +1,28 @@
 let number = document.getElementById("question_number")
 let question = document.getElementById("question")
 let points = 0
+let running = true
+let question_num = 0
+
+
+
+
+
 
 function one() {
-
+  number.innerText = +number.innerText + 1
+  
 }
 
 function two() {
-  
+  number.innerText = "Hej"
 }
 
 function three() {
-  
+  number.innerText = +number.innerText + 1
 }
 
 function four() {
-  
+  number.innerText = +number.innerText + 1
 }
+
