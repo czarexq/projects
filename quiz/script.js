d6ba7030f15920b27c06e75c8e66ab1752583e97
Adam Czarycki <adam.czarycki1@gmail.com>
2026-05-14 12:16:14 +0200

    Finish questions exept first

--- quiz/script.js
+++ quiz/script.js
@@ -21,15 +21,13 @@ if (+number.innerText == 1) {
   button3.innerText = "Hyperlink and Text Management";
   button4.innerText = "Home Tool Markup Language";
 }
-
-function second_qestion() {
+function second_question() {
   question.innerText = "2. In JavaScript, which keyword is used to declare a variable that can be changed?";
   button1.innerText = "const";
   button2.innerText = "let";
   button3.innerText = "var_change";
   button4.innerText = "fixed";
 }
-
 function third_question() {
   question.innerText = "3. What is the main purpose of CSS?";
   button1.innerText = "To create logic and functions";
@@ -37,39 +35,127 @@ function third_question() {
   button3.innerText = "To style and layout web pages";
   button4.innerText = "To send emails from a server";
 }
+function fourth_question() {
+  question.innerText = "4. Which Git command is used to copy a repository from GitHub to your local machine?";
+  button1.innerText = "git copy";
+  button2.innerText = "git clone";
+  button3.innerText = "git download";
+  button4.innerText = "git pull --all";
+}
+function fifth_question() {
+  question.innerText = "5. What is a 'Boolean' in programming?";
+  button1.innerText = "A type of loop";
+  button2.innerText = "A function that returns text";
+  button3.innerText = "A data type that can only be True or False";
+  button4.innerText = "A styling property in CSS";
+}
+function sixth_question() {
+  question.innerText = "6. Which Git command moves your local changes to the 'Staging Area'?";
+  button1.innerText = "git add";
+  button2.innerText = "git stage-now";
+  button3.innerText = "git commit";
+  button4.innerText = "git push";
+}
+function seventh_question() {
+  question.innerText = "7. What does 'Dry Run' mean in coding?";
+  button1.innerText = "Coding without drinking water";
+  button2.innerText = "Testing a process without actually executing the actions";
+  button3.innerText = "Running code on a server";
+  button4.innerText = "Writing code on paper";
+}
+function eighth_question() {
+  question.innerText = "8. In Git, what is the 'HEAD'?";
+  button1.innerText = "The top line of a file";
+  button2.innerText = "A pointer to the current branch/commit you are working on";
+  button3.innerText = "The first ever commit in a project";
+  button4.innerText = "The GitHub profile administrator";
+}
+function ninth_question() {
+  question.innerText = "9. What is an 'Array' used for?";
+  button1.innerText = "To store multiple values in a single variable";
+  button2.innerText = "To connect to the internet";
+  button3.innerText = "To delete files permanently";
+  button4.innerText = "To create a new folder";
+}
+function tenth_question() {
+  question.innerText = "10. What does the 'm' stand for in: git commit -m 'Message'?";
+  button1.innerText = "main";
+  button2.innerText = "modify";
+  button3.innerText = "message";
+  button4.innerText = "move";
+}
+
 
 
 function one(){
-  number.innerText = +number.innerText + 1;
-  if(+number.innerText == 1) {
-    count_score += 1
-    score.innerText = count_score;
-  }
-  if(+number.innerText == 2) {
-    second_qestion()
-  }
-  if(+number.innerText == 3) {
-    third_question()
-  }
-
-  if(+number.innerText == 10) {
-    hide()
-  }
+    number.innerText = +number.innerText + 1;
+    if(+number.innerText == 2) {
+      second_qestion()
+    }
+    if(+number.innerText == 3) {
+      third_question()
+    }
+    if(+number.innerText == 4) {
+      fourth_question()
+    }
+    if(+number.innerText == 5) {
+      fifth_question()
+    }
+    if(+number.innerText == 6) {
+      sixth_question()
+    }
+    if(+number.innerText == 7) {
+      seventh_question()
+    }
+    if(+number.innerText == 8) {
+      eighth_question()
+    }
+    if(+number.innerText == 9) {
+      ninth_question()
+    }
+    if(+number.innerText == 10) {
+      tenth_question()
+    }
+    if(+number.innerText == 11) {
+      number.innerText = 10
+      hide()
+      question.innerText = "Finished"
+    }
 }
 
 function two() {
     number.innerText = +number.innerText + 1;
     if(+number.innerText == 2) {
       second_qestion()
-      count_score += 1
-      score.innerText = count_score;
     }
     if(+number.innerText == 3) {
       third_question()
     }
-
+    if(+number.innerText == 4) {
+      fourth_question()
+    }
+    if(+number.innerText == 5) {
+      fifth_question()
+    }
+    if(+number.innerText == 6) {
+      sixth_question()
+    }
+    if(+number.innerText == 7) {
+      seventh_question()
+    }
+    if(+number.innerText == 8) {
+      eighth_question()
+    }
+    if(+number.innerText == 9) {
+      ninth_question()
+    }
     if(+number.innerText == 10) {
+      tenth_question()
+    }
+    if(+number.innerText == 11) {
+      number.innerText = 10
       hide()
+      question.innerText = "Finished"
     }
   }
 
@@ -81,22 +167,66 @@ function three() {
     if(+number.innerText == 3) {
       third_question()
     }
-
+    if(+number.innerText == 4) {
+      fourth_question()
+    }
+    if(+number.innerText == 5) {
+      fifth_question()
+    }
+    if(+number.innerText == 6) {
+      sixth_question()
+    }
+    if(+number.innerText == 7) {
+      seventh_question()
+    }
+    if(+number.innerText == 8) {
+      eighth_question()
+    }
+    if(+number.innerText == 9) {
+      ninth_question()
+    }
     if(+number.innerText == 10) {
+      tenth_question()
+    }
+    if(+number.innerText == 11) {
+      number.innerText = 10
       hide()
+      question.innerText = "Finished"
     }
   }  
 
 function four() {
-    number.innerText = +number.innerText + 1 ;
+    number.innerText = +number.innerText + 1;
     if(+number.innerText == 2) {
       second_qestion()
     }
     if(+number.innerText == 3) {
       third_question()
     }
-
+    if(+number.innerText == 4) {
+      fourth_question()
+    }
+    if(+number.innerText == 5) {
+      fifth_question()
+    }
+    if(+number.innerText == 6) {
+      sixth_question()
+    }
+    if(+number.innerText == 7) {
+      seventh_question()
+    }
+    if(+number.innerText == 8) {
+      eighth_question()
+    }
+    if(+number.innerText == 9) {
+      ninth_question()
+    }
     if(+number.innerText == 10) {
+      tenth_question()
+    }
+    if(+number.innerText == 11) {
+      number.innerText = 10
       hide()
+      question.innerText = "Finished"
     }
   } 
